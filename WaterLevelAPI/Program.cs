d0d5fb74bb7b809aa602e4d976971b538e3e63d8
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2026-06-27 15:31:19 -0300

    Habilitando Swagger em produção

--- WaterLevelAPI/Program.cs
+++ WaterLevelAPI/Program.cs
@@ -22,7 +22,9 @@ var app = builder.Build();
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwaggerUI( c => {
+    c.RoutePrefix = string.Empty;
+});
 //}
 
 app.UseHttpsRedirection();
